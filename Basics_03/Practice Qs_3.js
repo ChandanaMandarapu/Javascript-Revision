5f15cf597b9c39fcd82b2ec796dae8f9859f4568
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-08-04 08:40:24 +0530

    Practiced Conditionals

--- Basics_03/Practice Qs_3.js
+++ Basics_03/Practice Qs_3.js
@@ -27,4 +27,39 @@ function countPositiveNegativeZero(arr) {
     }
     return count;
   }
+
+  // Write a JavaScript function that takes two numbers as input and returns the greater number. 
+
+  function findGreaterNumber(num1, num2) {
+    if (num1 > num2) {
+      return num1;
+    } else if (num2 > num1) {
+      return num2;
+    } else {
+      return "Both numbers are equal.";
+    }
+  }
+  // Write a JavaScript function that takes an array of numbers as input and returns the sum of all even numbers in the array.
+
+  function sumEvenNumbers(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sum += arr[i];
+      }
+    }
+    return sum;
+  }
+
+  // Write a JavaScript function that takes a string as input and returns "Long" if the length of the string is greater than 10 characters, "Short" if it's less than 10 characters, and "Medium" if it's exactly 10 characters long.
+
+  function checkStringLength(str) {
+    if (str.length > 10) {
+      return "Long";
+    } else if (str.length < 10) {
+      return "Short";
+    } else {
+      return "Medium";
+    }
+  }
   
