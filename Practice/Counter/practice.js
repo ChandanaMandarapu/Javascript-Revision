37d5cb28d166116bfefefc0ce50720f0c35c19af
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-10-19 11:11:42 +0530

    practice | P2 - basic calci

---
+++ Practice/Counter/practice.js
@@ -0,0 +1,27 @@
+const firstName = "chandana"
+const lastName = " Mandarapu"
+console.log(firstName + lastName)
+
+const greeting = "Hi there"
+function greet() {
+    console.log(`${greeting}, ${firstName}`)
+}
+greet()
+
+let myPoints = 3
+
+const addThree = () => myPoints + 3
+console.log(addThree())
+
+const removeThree = () => myPoints - 3
+console.log(removeThree())
+
+console.log("2" + 2) //22
+console.log(11 + 7) //17
+console.log(6 + "5") // 65
+console.log("My points: " + 5 + 9) // 59
+console.log(2 + 2) // 4
+console.log("11" + "14") //  
+
+let num1 = 8
+let num2 = 2
