12c5539bc1b60c783b678f23e0f5d50a66b067d6
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-11 23:41:52 +0530

    learn asyncUtility.js

---
+++ Asynchronous Javascript/asyncUtility.js
@@ -0,0 +1,23 @@
+const mockAPICall = (data, delay) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(data);
+    }, delay);
+  });
+};
+
+const fetchDataConcurrently = async () => {
+  try {
+    const [result1, result2] = await Promise.all([
+      mockAPICall({ name: 'Data from API 1' }, 2000),
+      mockAPICall({ name: 'Data from API 2' }, 1500)
+    ]);
+
+    console.log('Result from API 1:', result1);
+    console.log('Result from API 2:', result2);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+fetchDataConcurrently();
