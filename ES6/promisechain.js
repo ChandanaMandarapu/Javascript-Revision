abf7d2919c842082244d7dd5fb30e392f9c59b20
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-12 23:23:51 +0530

    Update promisechain.js

--- ES6/promisechain.js
+++ ES6/promisechain.js
@@ -1,9 +1,27 @@
-const fetchData = (url) => {
-  return fetch(url).then((response) => response.json());
+// const fetchData = (url) => {
+//   return fetch(url).then((response) => response.json());
+// };
+
+// fetchData('/api/first')
+//   .then((data1) => fetchData(`/api/second/${data1.id}`))
+//   .then((data2) => fetchData(`/api/third/${data2.id}`))
+//   .then((data3) => console.log(data3))
+//   .catch((error) => console.error(error));
+
+
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 };
 
-fetchData('/api/first')
-  .then((data1) => fetchData(`/api/second/${data1.id}`))
-  .then((data2) => fetchData(`/api/third/${data2.id}`))
-  .then((data3) => console.log(data3))
-  .catch((error) => console.error(error));
+const displayData = async () => {
+  try {
+    const result = await fetchData('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(result);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+displayData();
