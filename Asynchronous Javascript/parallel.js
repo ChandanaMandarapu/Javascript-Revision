7674cb00eda8a628db25d4cfacba95cbca30ca6e
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-14 22:47:05 +0530

    Update parallel.js

--- Asynchronous Javascript/parallel.js
+++ Asynchronous Javascript/parallel.js
@@ -10,3 +10,20 @@ async function parallelAsyncOperations() {
 
 parallelAsyncOperations();
 
+// throtting
+
+function throttleAsync(fn, delay) {
+  let lastCall = 0;
+
+  return async function (...args) {
+    const now = Date.now();
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      return fn(...args);
+    }
+  };
+}
+
+const throttledAsyncFunction = throttleAsync(asyncOperation, 2000);
+throttledAsyncFunction(3);
+throttledAsyncFunction(7); // Will be ignored due to throttling
