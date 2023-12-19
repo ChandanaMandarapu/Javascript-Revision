3f40e1b055e34efbc181a35f6a9f0a8248c79b14
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-19 23:25:57 +0530

    Create LC03.js

---
+++ Practice/LC03.js
@@ -0,0 +1,29 @@
+function createCounter(init) {
+    let currentValue = init;
+
+    return {
+        increment: function () {
+            currentValue += 1;
+            return currentValue;
+        },
+        decrement: function () {
+            currentValue -= 1;
+            return currentValue;
+        },
+        reset: function () {
+            currentValue = init;
+            return currentValue;
+        }
+    };
+}
+
+// Example usage:
+const calls1 = ["increment", "reset", "decrement"];
+const counter1 = createCounter(5);
+const output1 = calls1.map(call => counter1[call]());
+console.log(output1); 
+
+const calls2 = ["increment", "increment", "decrement", "reset", "reset"];
+const counter2 = createCounter(0);
+const output2 = calls2.map(call => counter2[call]());
+console.log(output2);// Output: [1, 2, 1, 0, 0]
