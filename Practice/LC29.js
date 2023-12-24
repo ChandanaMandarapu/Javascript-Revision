e55329b9888c8ab08c71422d976ed115e17b2a71
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-24 23:48:12 +0530

    Update LC29.js

--- Practice/LC29.js
+++ Practice/LC29.js
@@ -1,1 +1,11 @@
+// Certainly! The EventEmitter class is like a tool that helps you manage events in your code. An event is something that happens, and you might want to do something when it occurs.
 
+// The subscribe method is like saying, "Hey, if this specific event happens, do something." You tell the tool the event's name (like "firstEvent") and what you want to happen (a function that does something). It gives you back a way to stop listening to that event if you want to later (this is the unsubscribe method).
+
+// The emit method is like making the event happen. If someone was listening (subscribed) to that event, their function gets called. You can also give some information to those functions when the event happens.
+
+// The example with actions and values is just showing you how to use this tool. You create the tool (new EventEmitter()), tell it what to do when an event happens (subscribe), make the event happen (emit), and maybe stop listening to an event (unsubscribe).
+
+// For example, you might have an event named "buttonClick." You subscribe to it, saying "when the button is clicked, do this." Later, when the button is clicked, you use emit to make that event happen, and the things you wanted to do when the button is clicked actually happen.
+
+// This tool is helpful when you have different parts of your program that need to know when certain things occur, like a button click, data arriving, or any other event in your program.
