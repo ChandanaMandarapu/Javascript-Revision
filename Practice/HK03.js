620be23cb32808d4fd735616e58089705b175547
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-25 22:15:47 +0530

    Update HK03.js

--- Practice/HK03.js
+++ Practice/HK03.js
@@ -1,1 +1,28 @@
+// // 
+// const nums = [2, 7, 11, 15];
+// const target = 9;
 
+// console.log(twoSum(nums, target)); // Should return [0, 1] or [1, 0]
+
+function twoSum(nums, target) {
+  const numMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
+    }
+
+    numMap.set(nums[i], i);
+  }
+
+  // No solution found
+  return [];
+}
+
+// Test the function
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+console.log(twoSum(nums, target)); // Should return [0, 1] or [1, 0]
