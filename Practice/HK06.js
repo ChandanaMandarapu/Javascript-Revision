02bcc8ae91e61de72c95c586d0ab33a246d5a122
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-26 23:17:59 +0530

    Update HK06.js

--- Practice/HK06.js
+++ Practice/HK06.js
@@ -1,1 +1,19 @@
+async function fetchDataAsync(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+    }
+}
+
+const apiUrlAsync = "https://jsonplaceholder.typicode.com/posts/1";
+fetchDataAsync(apiUrlAsync)
+    .then(data => {
+        console.log("Fetched data asynchronously:", data);
+    });
 
