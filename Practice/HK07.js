6ffd45698ffc852d5ec7eaaedc0e53f20203c38c
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-26 23:18:23 +0530

    Update HK07.js

--- Practice/HK07.js
+++ Practice/HK07.js
@@ -1,1 +1,15 @@
+async function delayedFunction(timeout) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`Function resolved after ${timeout} ms`);
+        }, timeout);
+    });
+}
+
+async function executeAsyncFunction() {
+    const result = await delayedFunction(2000);
+    console.log(result);
+}
+
+executeAsyncFunction();
 
