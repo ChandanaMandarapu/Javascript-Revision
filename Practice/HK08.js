d121dac23d6490760b93c9338dc4d8d13ef046c7
Chandana <123133294+ChandanaMandarapu@users.noreply.github.com>
2023-12-26 23:18:40 +0530

    Update HK08.js

--- Practice/HK08.js
+++ Practice/HK08.js
@@ -1,1 +1,26 @@
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        if (b !== 0) {
+            return a / b;
+        } else {
+            return "Cannot divide by zero";
+        }
+    }
+}
+
+const myCalculator = new Calculator();
+console.log(myCalculator.add(5, 3));
+console.log(myCalculator.multiply(4, 7));
 
